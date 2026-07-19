35d53328acd3be4de491b1d03aff6e1faffcc902
SignaInc <incsigna009@gmail.com>
2026-07-19 19:23:44 +0300

    Test Camera

--- sajdah_helper_ios/flutter_bootstrap.js
+++ sajdah_helper_ios/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "82460594" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2132039555" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
