3c43b6132835dee5d883746513ec9162bd3ee7a4
SignaInc <incsigna009@gmail.com>
2026-07-21 23:57:56 +0300

    Fix Bug On iOS 3.1

--- Sajdah_Helper_iOS/flutter_bootstrap.js
+++ Sajdah_Helper_iOS/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3798989068" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4166627596" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
