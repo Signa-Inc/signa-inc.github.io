72b0c50a05c462134e20b1610c77962aaac53e15
SignaInc <incsigna009@gmail.com>
2026-07-22 13:45:52 +0300

    Fix SH On iOS 6

--- sajdah_helper_ios/flutter_bootstrap.js
+++ sajdah_helper_ios/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "820756931" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1105333994" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
